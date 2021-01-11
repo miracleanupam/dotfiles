ef32656d4f3772862d0ea60b42a18d6e29f36656
Anupam Dahal <miracleanupam@gmail.com>
2021-01-11 11:26:37 +0200

    modified battery level notifier script; improved performance

--- custom_scripts/battery_level_notifier.sh
+++ custom_scripts/battery_level_notifier.sh
@@ -1,11 +1,26 @@
 #! /bin/bash
 
-while true
-do
-    BATTERY_PERC=$(cat /sys/class/power_supply/BAT0/capacity);
+CHECK_DELAY_IN_SECS=300
 
-    if [[ $BATTERY_PERC -lt 25 ]]; then
-        sudo -u anupam DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send -u critical -t 60000 "Battery Low, Plug In Power Source";
-    fi
-    sleep 300
-done
+while true
+    do
+        BATTERY_STATUS=$(cat /sys/class/power_supply/BAT0/status);
+
+        if [[ $BATTERY_STATUS -eq "Discharging" ]]; then
+            BATTERY_PERC=$(cat /sys/class/power_supply/BAT0/capacity);
+            if [[ $BATTERY_PERC -gt 50 ]]; then
+                CHECK_DELAY_IN_SECS=600;
+            elif [[ $BATTERY_PERC -lt 35 ]]; then
+                CHECK_DELAY_IN_SECS=300;
+            elif [[ $BATTERY_PERC -gt 30 ]]; then
+                CHECK_DELAY_IN_SECS=150;
+            elif [[ $BATTERY_PERC -lt 26 ]]; then
+                sudo -u anupam DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send -u critical -t $CHECK_DELAY_IN_SECS "Battery Low, Plug In Power Source";
+            fi
+            sleep $CHECK_DELAY_IN_SECS;
+
+        else
+            sleep $CHECK_DELAY_IN_SECS;
+            CHECK_DELAY_IN_SECS=600;
+        fi
+    done
