c22c19ad2f22d120264f813e82d024c67d6e7259
Anupam Dahal <miracleanupam@gmail.com>
2021-01-11 11:32:16 +0200

    modified battery level notifier, changed the comparision from -lt to -gt everywhere for better code organization

--- custom_scripts/battery_level_notifier.sh
+++ custom_scripts/battery_level_notifier.sh
@@ -8,15 +8,16 @@ while true
 
         if [[ $BATTERY_STATUS -eq "Discharging" ]]; then
             BATTERY_PERC=$(cat /sys/class/power_supply/BAT0/capacity);
-            if [[ $BATTERY_PERC -gt 50 ]]; then
+            if [[ $BATTERY_PERC -gt 35 ]]; then
                 CHECK_DELAY_IN_SECS=600;
-            elif [[ $BATTERY_PERC -lt 35 ]]; then
+            elif [[ $BATTERY_PERC -gt 30 ]]; then
                 CHECK_DELAY_IN_SECS=300;
-            elif [[ $BATTERY_PERC -lt 30 ]]; then
+            elif [[ $BATTERY_PERC -gt 25 ]]; then
                 CHECK_DELAY_IN_SECS=150;
-            elif [[ $BATTERY_PERC -lt 26 ]]; then
+            else
                 sudo -u anupam DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send -u critical -t $CHECK_DELAY_IN_SECS "Battery Low, Plug In Power Source";
             fi
+
             sleep $CHECK_DELAY_IN_SECS;
 
         else
