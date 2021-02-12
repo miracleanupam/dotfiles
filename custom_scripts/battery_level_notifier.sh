3c7d425893549792daea67862461e0e53e577b4b
Anupam Dahal <miracleanupam@gmail.com>
2021-02-12 10:27:37 +0200

    battery level notifier; changed the equality so that battery low notification does not come up when charging

--- custom_scripts/battery_level_notifier.sh
+++ custom_scripts/battery_level_notifier.sh
@@ -6,7 +6,7 @@ while true
     do
         BATTERY_STATUS=$(cat /sys/class/power_supply/BAT0/status);
 
-        if [[ $BATTERY_STATUS -eq "Discharging" ]]; then
+        if [[ "$BATTERY_STATUS" == "Discharging" ]]; then
             BATTERY_PERC=$(cat /sys/class/power_supply/BAT0/capacity);
             if [[ $BATTERY_PERC -gt 35 ]]; then
                 CHECK_DELAY_IN_SECS=600;
@@ -15,7 +15,8 @@ while true
             elif [[ $BATTERY_PERC -gt 25 ]]; then
                 CHECK_DELAY_IN_SECS=150;
             else
-                sudo -u anupam DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send -u critical -t $CHECK_DELAY_IN_SECS "Battery Low, Plug In Power Source";
+                CHECK_DELAY_IN_SECS=150;
+                sudo -u anupam DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send -u critical -t 149000 "Battery Low, Plug In Power Source";
             fi
 
             sleep $CHECK_DELAY_IN_SECS;
